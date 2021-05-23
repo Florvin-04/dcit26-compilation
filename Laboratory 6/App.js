09b9542b8561dae492ece58d743ee8dee9b30e2a
Florvin B <bayaborda64@gmail.com>
2021-05-23 09:54:43 +0800

    first commit

---
+++ Laboratory 6/App.js
@@ -0,0 +1,12 @@
+//BSCS 3-4
+//Bayaborda, Florvin
+//Eridao, James Conrad
+//Pascua, Aubrey
+import React from 'react';
+import Navigation from './routes/MainStack';
+
+export default function App(){
+  return(
+    <Navigation />
+  );
+}
